7b01501381f0849609c9c47731524daf81ad7d2a
Rebecca Strauss <rstrauss127@gmail.com>
2018-08-31 20:00:09 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,9 +1,14 @@
 class Project
 
-  attr_accessor :title
+  attr_accessor :title, :backers
 
   def initialize(title)
     @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
   end
 
 end
